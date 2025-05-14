0ddf1d1dbaadc03db5ab119972b502ea2c527ab0
八重垣艾莉卡 <1151203150@qq.com>
2025-05-14 16:39:41 +0800

    添加自动退出

--- main.go
+++ main.go
@@ -78,8 +78,21 @@ func main() {
 }
 
 func waitForExit() {
-	fmt.Println("\n签到执行完毕，按回车键退出程序...")
-	_, _ = fmt.Scanln()
+	fmt.Println("\n签到执行完毕，按回车键退出程序（30秒后自动退出）...")
+
+	inputCh := make(chan int)
+
+	go func() {
+		n, _ := fmt.Scanln()
+		inputCh <- n
+	}()
+
+	select {
+	case <-inputCh:
+		fmt.Println("程序退出")
+	case <-time.After(10 * time.Second): // 30秒超时
+		fmt.Println("\n等待超时，程序自动退出")
+	}
 }
 
 func tryAutoLogin(authAPI *api.AuthAPI, token string) *models.CredResult {
