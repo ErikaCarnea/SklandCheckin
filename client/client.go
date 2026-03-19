3857c19504aa2725d58a025381a25c3e64429d27
Erika Carnea <1151203150@qq.com>
2026-03-19 23:35:05 +0800

    清理冗余代码

--- client/client.go
+++ client/client.go
@@ -65,37 +65,6 @@ func (c *httpClient) SetSignToken(token string) {
 	c.signToken = token
 }
 
-// func (c *httpClient) doRequest(method, url string, body any, headers map[string]string, timeout time.Duration) (*http.Response, error) {
-// 	var reqBody io.Reader
-// 	if body != nil {
-// 		jsonBody, err := json.Marshal(body)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 		reqBody = bytes.NewBuffer(jsonBody)
-// 	}
-
-// 	req, err := http.NewRequest(method, url, reqBody)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	// 设置headers
-// 	for k, v := range headers {
-// 		req.Header.Set(k, v)
-// 	}
-
-// 	// 使用自定义超时（如果提供）
-// 	client := c.client
-// 	if timeout > 0 {
-// 		clientCopy := *c.client
-// 		clientCopy.Timeout = timeout
-// 		client = &clientCopy
-// 	}
-
-// 	return client.Do(req)
-// }
-
 func (c *httpClient) GetSignHeaders(urlStr, method string, bodyBytes []byte) (map[string]string, error) {
 	u, err := url.Parse(urlStr)
 	if err != nil {
